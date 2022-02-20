6d94379973e1d07ed6bbdc5e2cbe321ff89123ef
hadar <hzeigerson@gmail.com>
2022-02-20 11:48:32 -0700

    monthly reports 6

--- monthly_reports_requests_project/main.py
+++ monthly_reports_requests_project/main.py
@@ -2,7 +2,8 @@ from distutils.log import error
 import logging as lg
 import os
 import shutil
-import openpyxl
+import pandas as pd
+# import openpyxl
 import datetime
 
 
@@ -71,41 +72,65 @@ def getInfo(filename):
 
     #### check file formatting ###
     try:
-        # extract month from title, check formatting
+        # extract month from title, error check formatting
         for key in month_dict:
             if key in filename:
                 month = key
                 month_val = month_dict[key]
         
-        # extract year from title, check formatting
+        # extract year from title, error check formatting
         year = int(''.join(list(filter(str.isdigit, filename))))
-        if len(str(year)) != 4 or str(year)[:3] != '201':
+        if len(str(year)) != 4 or str(year)[:2] != '20':
             raise FileNameError
 
-        # check general formatting
+        # error check general formatting
         accepted_format = "MonthlyReports\expedia_report_monthly_{}_{}.xlsx".\
             format(month, year)
         lg.debug("File Name:{} \nExpected Format:{}".format(filename, accepted_format))
         if filename == accepted_format:
-            lg.info("File is in expected format")
+            lg.info("File is in'VOC Rolling MoM' expected format")
             pass
         else:
             raise FileNameError
         
-        # check if file in file.lst
-        with open("file.lst", "a") as lst_file:
+        # error check if file in file.lst
+        with open("file.lst", "r") as lst_file:
             if os.stat("file.lst").st_size != 0: 
-                contents = lst_file.readlines()
-                if file in contents:
+                contents = lst_file.read()
+                if filename in contents:
                     lg.debug("File '{}' has been already processed".format(filename))
                     raise DuplicateError
+                else: pass
+            else: pass
 
+        ### opening file ###
+        
+        # obtaining dataframes from various tabs#
+        summary_df = pd.read_excel(
+            filename, 
+            sheet_name="Summary Rolling MoM",
+            engine="openpyxl"
+            )
+        lg.info('Summary report file opened.')
+        
+        print(summary_df)
 
+        promoter_df = pd.read_excel(
+            filename, 
+            sheet_name="VOC Rolling MoM",
+            engine="openpyxl"
+            )
+        lg.info('Summary report file opened.')
 
+        print(promoter_df)
 
+        # report task for 'Summary Rolling MoM' 
 
-        lst_file.write(filename)
-        lst_file.close()
+        # report task for 'VOC Rolling MoM'
+
+        with open("file.lst", "a") as lst_file:
+            lst_file.writelines("\n" + filename)
+            lst_file.close()
 
     except UnboundLocalError:
         lg.info("File MONTH is not in expected format. Moving to error folder.")
@@ -128,8 +153,6 @@ def getInfo(filename):
 
 
 
-   
-
 #     # input monthly report request
 #     # date_str = '{},{}'.format(month, year)
 #     datetime_obj = datetime.datetime.strptime(month+"-"+year, "%B-%Y")
@@ -138,15 +161,7 @@ def getInfo(filename):
 #     # list of processed files
 #     # file_lst =  
 
-#     # filename = "MonthlyReports/expedia_report_monthly_{}_{}.xlsx".format(month, year)
-#     # lg.debug('Opening file {}'.format(filename))
 
-#     report_df = pd.read_excel(
-#         filename, 
-#         sheet_name="Summary Rolling MoM",
-#         engine="openpyxl"
-#         )
-#     lg.info('File opened')
 
 
 #     lg.debug('Collecting Report Features')
@@ -221,12 +236,12 @@ elif request_ask == 'n' or request_ask == 'no' or request_ask == '':
     lg.info("Exhaustive Report Request for Reports in {}".format(directory))
     print("Exhaustive Report Request in Processing")
     counter = 0
+    total = len([item for item in os.listdir('MonthlyReports')])
     for filename in os.listdir(directory):
         file = os.path.join(directory, filename)
         if os.path.isfile(file):
             getInfo(file)
             counter += 1
-            total = len([item for item in os.listdir('.')])
             lg.info('File {}/{} Processed'.format(str(counter), str(total)))
         else:
             raise FileNotFoundError
--- monthly_reports_requests_project/tests.py
+++ monthly_reports_requests_project/tests.py
@@ -4,9 +4,18 @@ import os
 # if 10 in range(5):
 #     print(True)
 
-# filename = 'MonthlyReports\expedia_report_monthly_march_2018.xlsx'
+filename = 'MonthlyReports\expedia_report_monthly_march_2018.xlsx'
 # year = int(''.join(list(filter(str.isdigit, filename))))
 # print(year)
 
 # month = 4
-print(len(str(12345)))
+# print(len(str(12345)))
+
+with open("file.lst", "r") as lst_file:
+    if os.stat("file.lst").st_size != 0: 
+        contents = lst_file.read()
+        if filename in contents:
+            print("File '{}' has been already processed".format(filename))
+            raise Exception("duplicate error")
+        else: pass
+    else: pass
