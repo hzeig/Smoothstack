d3d8cd474ed9e9d1e9eb2ee31c2b4fac691de182
hadar <hzeigerson@gmail.com>
2022-02-20 10:13:10 -0700

    monthly reports 5

--- monthly_reports_requests_project/main.py
+++ monthly_reports_requests_project/main.py
@@ -1,6 +1,8 @@
-import os
-import pandas
+from distutils.log import error
 import logging as lg
+import os
+import shutil
+import openpyxl
 import datetime
 
 
@@ -13,6 +15,19 @@ lg.basicConfig(
     format = log_format, 
     level = lg.DEBUG
     )
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+class FileNameError(Error):
+    """Raised when file format is incorrect"""
+    pass
+
+class DuplicateError(Error):
+    """Raised when file has already been processed \
+        and is listed in file.lst"""
+
 
 ########################### REPORT REQUEST FUNCTIONS #######################
 
@@ -34,39 +49,86 @@ lg.basicConfig(
 
 
 def getInfo(filename):
-    lg.info('Request Initiated')
+    lg.info("getInfo() Function Start")
 
     month_dict = {
-        "January":  1,
-        "February": 2,
-        "March":    3,
-        "April":    4,
-        "May":      5,
-        "June":     6,
-        "July":     7,
-        "August":   8,
-        "September":9,
-        "October":  10,
-        "November": 11,
-        "December": 12
+        "january":  1,
+        "february": 2,
+        "march":    3,
+        "april":    4,
+        "may":      5,
+        "june":     6,
+        "july":     7,
+        "august":   8,
+        "september":9,
+        "october":  10,
+        "november": 11,
+        "december": 12
     }
 
+    error_directory = "ErrorFiles"
+
+
+    #### check file formatting ###
+    try:
+        # extract month from title, check formatting
+        for key in month_dict:
+            if key in filename:
+                month = key
+                month_val = month_dict[key]
+        
+        # extract year from title, check formatting
+        year = int(''.join(list(filter(str.isdigit, filename))))
+        if len(str(year)) != 4 or str(year)[:3] != '201':
+            raise FileNameError
+
+        # check general formatting
+        accepted_format = "MonthlyReports\expedia_report_monthly_{}_{}.xlsx".\
+            format(month, year)
+        lg.debug("File Name:{} \nExpected Format:{}".format(filename, accepted_format))
+        if filename == accepted_format:
+            lg.info("File is in expected format")
+            pass
+        else:
+            raise FileNameError
+        
+        # check if file in file.lst
+        with open("file.lst", "a") as lst_file:
+            if os.stat("file.lst").st_size != 0: 
+                contents = lst_file.readlines()
+                if file in contents:
+                    lg.debug("File '{}' has been already processed".format(filename))
+                    raise DuplicateError
+
+
+
+
+
+        lst_file.write(filename)
+        lst_file.close()
+
+    except UnboundLocalError:
+        lg.info("File MONTH is not in expected format. Moving to error folder.")
+        if not os.path.exists(error_directory):
+            os.mkdir(error_directory)
+        shutil.move(filename, error_directory)
+        lg.info("File moved to error folder.")
+            
+
+    except FileNameError:
+        lg.info("File NAME is not in expected format. Moving to error folder.")
+        if not os.path.exists(error_directory):
+            os.mkdir(error_directory)
+        shutil.move(filename, error_directory)
+        lg.info("File moved to error folder.")
     
-    # check file formatting
-    accepted_format = "MonthlyReports/expedia_report_monthly_*.xlsx"
-    # # accepted_format = accepted_format.format(month=[x for x in month_dict.keys], year=)
-    # if filename == accepted_format:
-    #     print(True)
-    # else:
+    except DuplicateError:
+        lg.info("File has already been processed.")
+        lg.info("Skipping report.")        
 
 
-    # check if file in file.lst
-#     with open("file.lst", 'a') as lst_file:
-#         os.system(lst_file)
-#         contents = lst_file.read()
-#         if file in contents:
-#             lg.debug("File '{}' has been already processed".format(filename))
-#             break
+
+   
 
 #     # input monthly report request
 #     # date_str = '{},{}'.format(month, year)
@@ -134,30 +196,44 @@ def getInfo(filename):
 #     lg.info('Information Printed')
 #     # print("Report for {}, {}:".format(month, year))
 #     # print(final_df)
-#     lg.info('Request Complete')
+    lg.info('getInfo() Function End')
 
 
 ###################### REPORT REQUEST PROGRAM ##################################
 
-lg.info("Request Initiated")
+lg.info("*********Request Initiated*********")
 directory = "MonthlyReports"
 
 request_ask = input("Request a specific file? [y/[n]]").lower()
 if request_ask == 'y' or request_ask == 'yes':
     filename = input("Full file name:") 
-    lg.info("Specific Report Request for File: {}".format(file))
-    print("Requesting report for file: {}".format(file))
+    lg.info("Specific Report Request for File: {}".format(filename))
+    print("Requesting report for file: {}".format(filename))
     file = os.path.join(directory, filename)
     if os.path.isfile(file):
         getInfo(file)
+        lg.info('File Processed')
+        print("Request Complete")
+    else:
+        raise FileNotFoundError
 # Create general report for 'no' or Enter
 elif request_ask == 'n' or request_ask == 'no' or request_ask == '': 
     lg.info("Exhaustive Report Request for Reports in {}".format(directory))
-    print("Requesting Exhaustive Report")
+    print("Exhaustive Report Request in Processing")
+    counter = 0
     for filename in os.listdir(directory):
         file = os.path.join(directory, filename)
         if os.path.isfile(file):
             getInfo(file)
+            counter += 1
+            total = len([item for item in os.listdir('.')])
+            lg.info('File {}/{} Processed'.format(str(counter), str(total)))
+        else:
+            raise FileNotFoundError
 # Ask again if other input
 else:
-    Exception("Input is Incorrect. Try again.") 
+    raise Exception("Input is incorrect. Please restart program.") 
+
+
+lg.info('*********Request Complete*********')
+print("Request Complete")
---
+++ monthly_reports_requests_project/tests.py
@@ -0,0 +1,12 @@
+import os
+
+
+# if 10 in range(5):
+#     print(True)
+
+# filename = 'MonthlyReports\expedia_report_monthly_march_2018.xlsx'
+# year = int(''.join(list(filter(str.isdigit, filename))))
+# print(year)
+
+# month = 4
+print(len(str(12345)))
