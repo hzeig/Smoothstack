7671db17b49a74fd795ceba9e883f7a029918e97
hadar <hzeigerson@gmail.com>
2022-02-21 18:45:55 -0700

    monthly reports 11

--- monthly_reports_requests_project/summaryReport.py
+++ monthly_reports_requests_project/summaryReport.py
@@ -25,18 +25,21 @@ def summaryReport(path, date_obj):
     ws = wb["Summary Rolling MoM"]
     lg.info("Opened worksheet 'Summary Rolling Mom'.")
     
-    lg.info("Gathering report data.")
-    reg_date_format = "{}, {}".format(date_obj[0], date_obj[1])
+    
+    date = "{}, {}".format(date_obj[0], date_obj[1])
+    lg.info("Gathering report data for {}".format(date.capitalize()))
     data = []
     try:
-        lg.info("Locating file date.")
+        lg.info("Locating data row for {}.".format(date.capitalize()))
         for column in ws:
             for cell in column:
                 if isinstance(cell.value, datetime.date):
                     cell_date = cell.value.strftime("%B, %Y")
-                    if cell_date.lower() == reg_date_format:
+                    lg.debug("Comparing {} to {}".format(cell_date, date))
+                    if cell_date.lower() == date:
                         row = cell.row
-                        lg.info("Date found in row {} of file.".format(row))
+                        lg.info("{} found in row {} of file.".format(date.capitalize(), row))
+                        break
 
 
         lg.info("Collecting data.")
@@ -51,7 +54,7 @@ def summaryReport(path, date_obj):
                 FCR: {}% \n\
                 DSAT: {}% \n\
                 CSAT: {}% "\
-                .format(cell_date, *data)
+                .format(date.capitalize(), *data)
         lg.info("Data successfully gathered.")
         lg.debug(report)
         
